ff19e8da9755180e61d7b67aa10b77ecfd621458
farnoudlavasani-dev <lavasanifarnoud@gmail.com>
2024-09-02 08:54:51 +0330

    complete structure of first section

--- main.py
+++ main.py
@@ -2,22 +2,37 @@
 def greeting():
     username = input("Enter your username: ")
     print(f"Hello {username}, welcome to this terminal")
+    
+    
 def check_prime(thenumber):
-    if thenumber % 
+    is_prime = True
+    for i in range(2,thenumber):
+        if thenumber % i == 0:
+            is_prime = False
+    return is_prime
+
+def checking_prime_user(useranswerprimecheck):
+    if check_prime(useranswerprimecheck) == True:
+        print("great")
+    elif check_prime(useranswerprimecheck) == False:
+        print("this is not prime")
 
 def prime_numbers():
-    useranswer = input("Do you know anything about prime numbers? yes/no ")
-    if "yes" in useranswer:
-        useranswerprimecheck = int(input("can you say one? "))
-        if useranswerprimecheck == "":
-            check_prime(useranswerprimecheck)
-    if "no" in useranswer:
-        print("you should say one. ")
-    else:
-        print("you should say one. ")
+    try:
+        useranswer = input("Do you know anything about prime numbers? yes/no ")
+        if "yes" in useranswer:
+            useranswerprimecheck = int(input("can you say one? "))
+            checking_prime_user(useranswerprimecheck)
+        elif "no" in useranswer:
+            useranswerprimecheck = int(input("you should say one. "))
+            checking_prime_user(useranswerprimecheck)    
+        else:
+            useranswerprimecheck = int(input("you should say one. "))
+            checking_prime_user(useranswerprimecheck)
+    except ValueError:
+        print('please enter a number')
 
-def section_1():
+while True:
     greeting()
-    print("y")
-     
-section_1()
+    prime_numbers()
+    break
